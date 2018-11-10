5b776fa767635c0d2c39e2f275b1a5ca46d303df
Andrew J Wagner <845683+drewag@users.noreply.github.com>
2018-11-09 22:13:05 -0700

    [UP] Update to SQL 1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/drewag/swift-sql.git", from: "0.0.0"),
+        .package(url: "https://github.com/drewag/swift-sql.git", from: "1.0.0"),
         .package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3"),
     ],
     targets: [
