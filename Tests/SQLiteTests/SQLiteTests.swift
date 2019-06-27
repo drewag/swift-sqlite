b0822ab8b10e001e6ada76fd5ed0a4e2971443ca
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-06-27 12:58:41 -0600

    [CI] Add Xcode project

--- Tests/SQLiteTests/SQLiteTests.swift
+++ Tests/SQLiteTests/SQLiteTests.swift
@@ -3,10 +3,6 @@ import XCTest
 
 class SQLiteTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SQLite().text, "Hello, World!")
     }
 
 
