4644fd0815e29cdef78f6a0a44b5dddd98d17feb
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-07-24 15:47:54 -0600

    [UP] Update to SwiftSQL 4

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SQLite",
+    platforms: [.iOS(.v8), .macOS(.v10_11), .tvOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/drewag/swift-sql.git", from: "3.0.0"),
+        .package(url: "https://github.com/drewag/swift-sql.git", from: "4.0.0"),
         .package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3"),
     ],
     targets: [
